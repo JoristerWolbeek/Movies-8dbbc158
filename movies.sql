36325bed531966ff884244ea7886bd3188853ca7
Joris ter Wolbeek <21074092@talnet.nl>
2020-03-02 13:10:44 +0100

    Uitwerking van de exercise

---
+++ movies.sql
@@ -0,0 +1,25 @@
+CREATE TABLE movies (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    titel varchar(255) NOT NULL,
+    duur varchar(225) NOT NULL,
+    uitkomstDatum DATE,
+    landVanAfkomst varchar(255) NOT NULL,
+    omschrijving varchar(255) NOT NULL,
+    Trailer varchar(255) 
+);
+INSERT INTO movies (titel, duur, uitkomstDatum, landVanAfkomst, omschrijving, Trailer)
+VALUES ("SONIC: The Hedhehog", "93 minuten", DATE("2017-06-15"), "USA" "After discovering a small, blue, fast hedgehog, a small-town police officer must help it defeat an evil genius who wants to do experiments on it." "=4mW9FE5ILJs");
+
+INSERT INTO movies (titel, duur, uitkomstDatum, landVanAfkomst, omschrijving, Trailer)
+VALUES ("De Club van Sinterklaas", "85 minuten", DATE("2010-11-11"), "NL", "A horror film that depicts St. Nicholas as a murderous bishop who kidnaps and murders children when there is a full moon on December 5.", "=Xv3G70mm18k");
+
+INSERT INTO movies (titel, duur, uitkomstDatum, landVanAfkomst, omschrijving, Trailer)
+VALUES ("Kamen raidaa Jei", "45 minuten", DATE("1994-04-16"), "JP", "The story begins with three Aliens in human form preparing for a ritual to feed the Fog Mother's 'siblings'. The Fog Mother last came to earth during the Prehistoric Age and is the cause of the extinction of the dinosaurs. Kenji (KR J) and his kid sister are in the mountains, Kenji is investigating all the deaths of animals and trees there. The Fog Mother sends the three aliens, 'Prince Gorai', 'Zoo', and 'Agito' to kidnap Kenji's baby sister for the sacrifice. During the course, 'Agito' punches through Kenji's stomach and plunges to his death. Or did he?", "=-P2c-JuWCMQ");
+
+INSERT INTO movies (titel, duur, uitkomstDatum, landVanAfkomst, omschrijving, Trailer)
+VALUES ("Sherlock Gnomes", "86 minuten", DATE("2018-04-18"), "UK", "Garden gnomes, Gnomeo (James McAvoy) and Juliet (Emily Blunt), recruit renowned detective Sherlock Gnomes (Johnny Depp) to investigate the mysterious disappearance of other garden ornaments.", "=AboBbQ8Yztw");
+
+INSERT INTO movies (titel, duur, uitkomstDatum, landVanAfkomst, omschrijving, Trailer)
+VALUES ("The Room", "96 minuten", DATE("2004-03-3"), "USA", "Johnny is a successful bank executive who lives quietly in a San Francisco townhouse with his fiancée, Lisa. One day, putting aside any scruple, she seduces Johnny's best friend, Mark. From there, nothing will be the same again.", "=9-dIdFXeFhs");
+
+
